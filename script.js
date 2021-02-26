5c639ac7781ba944baf42087c10b9c0d2af7d3bc
Almar <almar.asetbekov@gmail.com>
2021-02-27 01:05:05 +0600

    contact page added

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 const mainNavigation = document.querySelector('.main-navigation'),
-      mainNaivgationLink = mainNavigation.querySelector('.active-link')
+	  mainNaivgationLinks = [...mainNavigation.querySelectorAll('.main-nav-link')]
+
+
+// function buildMailto(recipient, subject, body) {
+// 	var mailToLink = "mailto:" + recipient + "?";
+// 	var mailContent = "Subject=" + subject + "&";
+// 	mailContent += "cc=testemail@gmail.com&";
+// 	mailContent += "body="
+// 	mailContent += encodeURIComponent(body);
+// 	document.getElementById('myLink').addEventListener('click', function () {
+// 		window.location.href = mailToLink + mailContent;
+// 		return false;
+// 	});
+// }
+
+// buildMailto('almar.asetbekov@gmail.com', 'a subject', 'mail content')
+
 
 $('.submit').on("click", function(e) {
-    e.preventDefault()
-    let subject = $('#email').val(),
-        body = $('#body').val()
-    window.location.href = "mailto:almar.asetbekov@gmail.com?subject=" + subject + "&body=" + body
+	e.preventDefault()
+	let subject = $('#email').val(),
+		body = $('#body').val()
+	window.location.href = "mailto:almar.asetbekov@gmail.com?subject=" + subject + "&body=" + body
 })
