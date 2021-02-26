b8177abf98b2ac39941cd1e44d308409d5c120d4
Almar <almar.asetbekov@gmail.com>
2021-02-26 20:53:59 +0600

    added new page

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 const mainNavigation = document.querySelector('.main-navigation'),
       mainNaivgationLink = mainNavigation.querySelector('.active-link')
 
+$('.submit').on("click", function(e) {
+    e.preventDefault()
+    let subject = $('#email').val(),
+        body = $('#body').val()
+    window.location.href = "mailto:almar.asetbekov@gmail.com?subject=" + subject + "&body=" + body
+})
