f879af8dabe732306f21155e6fdfe13c7a477675
Almar <almar.asetbekov@gmail.com>
2021-02-28 16:17:45 +0600

    contact page finished

--- script.js
+++ script.js
@@ -1,25 +1,6 @@
-const mainNavigation = document.querySelector('.main-navigation'),
-	  mainNaivgationLinks = [...mainNavigation.querySelectorAll('.main-nav-link')]
+const contactForm = document.querySelector('#contact-form'),
+      contactFormMessage = contactForm.querySelector('.message')
 
-
-// function buildMailto(recipient, subject, body) {
-// 	var mailToLink = "mailto:" + recipient + "?";
-// 	var mailContent = "Subject=" + subject + "&";
-// 	mailContent += "cc=testemail@gmail.com&";
-// 	mailContent += "body="
-// 	mailContent += encodeURIComponent(body);
-// 	document.getElementById('myLink').addEventListener('click', function () {
-// 		window.location.href = mailToLink + mailContent;
-// 		return false;
-// 	});
-// }
-
-// buildMailto('almar.asetbekov@gmail.com', 'a subject', 'mail content')
-
-
-$('.submit').on("click", function(e) {
-	e.preventDefault()
-	let subject = $('#email').val(),
-		body = $('#body').val()
-	window.location.href = "mailto:almar.asetbekov@gmail.com?subject=" + subject + "&body=" + body
+contactFormMessage.addEventListener('change', function(e) {
+		e.target.value.trim().length === 0 && alert("Please enter your message.")
 })
