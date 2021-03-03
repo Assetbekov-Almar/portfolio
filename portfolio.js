a9659cf9b14d7187745b44de6b97c5b335418e5b
Almar <almar.asetbekov@gmail.com>
2021-03-04 00:20:16 +0600

    modified portfolio

---
+++ portfolio.js
@@ -0,0 +1,10 @@
+const portfolio = document.querySelector('.portfolio'),
+      portfolioItemBg = [...portfolio.querySelectorAll('.wrapper__item-bg')]
+
+portfolioItemBg.forEach(itemBg => {
+	itemBg.addEventListener('touchstart', function(e) {
+		e.preventDefault()
+		this.querySelector('.icons-wrapper').style.display = 'block'
+		this.querySelector('.links-wrapper').style.display = 'block'
+	})
+})
