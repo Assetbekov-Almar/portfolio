65cc96d2ec469508bca018822695905530935a63
Almar <almar.asetbekov@gmail.com>
2021-03-03 13:00:29 +0600

    adaptive added

---
+++ burgerMenu.js
@@ -0,0 +1,19 @@
+const burgerMenuElements = [...document.querySelectorAll('.burger-menu-element')],
+      burgerMenuBtn = document.querySelector('.header-burger-menu')
+
+burgerMenuBtn.addEventListener('click', function () {
+	burgerMenuElements.forEach(element => {
+		element.classList.add('burger-menu-open')
+	})
+	this.style.display = "none"
+})
+
+window.addEventListener('click', (e) => {
+	let modal = e.target
+	if (modal.classList.contains('header-burger-navbar-bg')) {
+		burgerMenuElements.forEach(element => {
+			element.classList.remove('burger-menu-open')
+		})
+		burgerMenuBtn.style.display = "block"
+	}
+})
