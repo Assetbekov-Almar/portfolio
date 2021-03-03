9aba6eb39527dd88962f0295cc2b1cab79612350
Almar <almar.asetbekov@gmail.com>
2021-03-04 01:40:37 +0600

    portfolio js modified

--- portfolio.js
+++ portfolio.js
@@ -1,14 +1,17 @@
-// const portfolio = document.querySelector('.portfolio'),
-//       portfolioItemBg = [...portfolio.querySelectorAll('.wrapper__item-bg')]
+const portfolio = document.querySelector('.portfolio'),
+      portfolioItemBg = [...portfolio.querySelectorAll('.wrapper__item-bg')],
+	  portfolioEvents = ['touchstart', 'click']
 
-// portfolioItemBg.forEach(itemBg => {
-// 	itemBg.addEventListener('touchstart click', function(e) {
-// 		console.log(this)
-// 		portfolioItemBg.forEach(item => {
-// 			item.querySelector('.icons-wrapper').style.display = 'none'
-// 			item.querySelector('.links-wrapper').style.display = 'none'
-// 		})
-// 		this.querySelector('.icons-wrapper').style.display = 'block'
-// 		this.querySelector('.links-wrapper').style.display = 'block'
-// 	})
-// })
+portfolioItemBg.forEach(itemBg => {
+	portfolioEvents.forEach(event => {
+		itemBg.addEventListener(event, function(e) {
+			e.preventDefault()
+			portfolioItemBg.forEach(item => {
+				item.querySelector('.icons-wrapper').style.display = 'none'
+				item.querySelector('.links-wrapper').style.display = 'none'
+			})
+			this.querySelector('.icons-wrapper').style.display = 'block'
+			this.querySelector('.links-wrapper').style.display = 'block'
+		})
+	})
+})
