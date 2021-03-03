716621bb3f2c152913d9d1fb3dcb0ad94b418acf
Almar <almar.asetbekov@gmail.com>
2021-03-04 00:27:34 +0600

    modified portfoliio

--- portfolio.js
+++ portfolio.js
@@ -1,10 +1,10 @@
-const portfolio = document.querySelector('.portfolio'),
-      portfolioItemBg = [...portfolio.querySelectorAll('.wrapper__item-bg')]
+// const portfolio = document.querySelector('.portfolio'),
+//       portfolioItemBg = [...portfolio.querySelectorAll('.wrapper__item-bg')]
 
-portfolioItemBg.forEach(itemBg => {
-	itemBg.addEventListener('touchstart', function(e) {
-		e.preventDefault()
-		this.querySelector('.icons-wrapper').style.display = 'block'
-		this.querySelector('.links-wrapper').style.display = 'block'
-	})
-})
+// portfolioItemBg.forEach(itemBg => {
+// 	itemBg.addEventListener('touchstart', function(e) {
+// 		e.preventDefault()
+// 		this.querySelector('.icons-wrapper').style.display = 'block'
+// 		this.querySelector('.links-wrapper').style.display = 'block'
+// 	})
+// })
