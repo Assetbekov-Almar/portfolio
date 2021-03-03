1ed17d55b69793893510605355d0cefc38fd65d1
Almar <almar.asetbekov@gmail.com>
2021-03-04 01:32:06 +0600

    hover mobile effect

--- portfolio.js
+++ portfolio.js
@@ -2,8 +2,12 @@
 //       portfolioItemBg = [...portfolio.querySelectorAll('.wrapper__item-bg')]
 
 // portfolioItemBg.forEach(itemBg => {
-// 	itemBg.addEventListener('touchstart', function(e) {
-// 		e.preventDefault()
+// 	itemBg.addEventListener('touchstart click', function(e) {
+// 		console.log(this)
+// 		portfolioItemBg.forEach(item => {
+// 			item.querySelector('.icons-wrapper').style.display = 'none'
+// 			item.querySelector('.links-wrapper').style.display = 'none'
+// 		})
 // 		this.querySelector('.icons-wrapper').style.display = 'block'
 // 		this.querySelector('.links-wrapper').style.display = 'block'
 // 	})
